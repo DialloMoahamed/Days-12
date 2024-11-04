ff8829463e46de8de2ccb24fd8f221e6770050b6
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-11-04 12:34:52 +0100

    Question 1 terminer

---
+++ Exo1.js
@@ -0,0 +1,9 @@
+// Reponse numero 1
+const txt = 'Il gagne 4000 euros de salaire par mois, 10000 euros de bonus annuel, 5500 euros de cours en ligne par mois. '
+let salaire = 4000 
+let bonus = 10000
+let cours = 5500
+let revenuAnuelTotal = salaire + bonus + cours
+console.log("revenu anuel total :" ,revenuAnuelTotal, "euro");
+
+
